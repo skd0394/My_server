05567e91436e8eb24fcc592b64c443f769017dd3
shivkant dubey <shivkd1908@gmail.com>
2022-09-26 04:25:37 +0530

    added the users

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,22 +1,25 @@
 var url = "https://shiv-mock-server.herokuapp.com/api"
 
 let Register = async (event) => {
+    event.preventDefault()
     var name = document.getElementById('name').value;
     var batch = document.getElementById('batch').value;
     var course = document.getElementById('course').value;
     var age = document.getElementById('age').value;
-    var mobile = document.getElementById('mobile').value;
+    var mobile = document.getElementById('mobile_no.').value;
     var score = document.getElementById('score').value;
     let userData = {
         name, batch, course, age, mobile, score
     }
-    event.preventDefault()
+    console.log(userData)
+
     let res = await fetch(`${url}/users`, {
         method: 'POST',
         body: JSON.stringify(userData),
         headers: {
             'Content-Type': "application/json"
-        }
-    })
-    window.location.href = `${url}/users`
+        },
+
+    });
+    // console.log(res)
 }
